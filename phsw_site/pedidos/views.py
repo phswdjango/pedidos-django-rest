5ebafcd8797d496d67b00c6b210c975e5843cfb7
Pedro Madureira <dev@phsolucoesweb.com.br>
2021-08-16 17:01:43 -0300

    criado view de pedidos por usuario. Criado facade da app pedidos.

--- phsw_site/pedidos/views.py
+++ phsw_site/pedidos/views.py
@@ -1,20 +1,10 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
-from phsw_site.pedidos.models import Pedido
-from django.db.models import Prefetch
+from . import facade
 
 @login_required
 def pedidos(request):
-    usuario = User.objects.all()
-    empresa = 
-    status = 
-    pedidos = Pedido.objects.order_by('data_pedido').prefetch_related(
-        Prefetch(), Prefetch(), Prefetch()
-    ).all()
-    return render(request, 'pedidos/pedidos.html', context={'pedidos': pedidos})
+    pedidos_do_usuario = facade.buscar_pedidos_do_usuario(request)
+    return render(request, 'pedidos/pedidos.html', context={'pedidos': pedidos_do_usuario})
 
 
-# def trazer_modulos_com_aulas():  # prefetch_related serve pra evitar o N+1 em busca do lado 'N' pro lado '1'
-#     aulas_ordenadas = Aula.objects.order_by('order')
-#     return Modulo.objects.order_by('order').prefetch_related(
-#         Prefetch('aula_set', queryset=aulas_ordenadas, to_attr='aulas')).all()
