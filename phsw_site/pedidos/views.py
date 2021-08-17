c70602930de9af64b7d29489b7c650cd5f49f12b
Pedro Madureira <dev@phsolucoesweb.com.br>
2021-08-17 16:50:41 -0300

    Criado pesquisa de pedidos por termo. Adicionado paginação na pagina de pedidos. Adicionado cores para status de pedidos.

--- phsw_site/pedidos/views.py
+++ phsw_site/pedidos/views.py
@@ -6,17 +6,8 @@ from django.core.paginator import Paginator
 
 @login_required
 def pedidos(request):
-    if request.user.is_agente_admin:
-        pedidos_do_usuario = facade.buscar_pedidos_do_agente_admin(request)
-        paginator = Paginator(pedidos_do_usuario, 3)
-        page = request.GET.get('page')
-        pedidos_paginator = paginator.get_page(page)
-
-        return render(request, 'pedidos/pedidos_agente_admin.html', context={'pedidos': pedidos_paginator})
-    pedidos_do_usuario = facade.buscar_pedidos_do_usuario(request)
-    paginator = Paginator(pedidos_do_usuario, 3)
+    pedidos_queryset = facade.buscar_pedidos(request)
+    paginator = Paginator(pedidos_queryset, 3)
     page = request.GET.get('page')
     pedidos_paginator = paginator.get_page(page)
     return render(request, 'pedidos/pedidos.html', context={'pedidos': pedidos_paginator})
-
-
