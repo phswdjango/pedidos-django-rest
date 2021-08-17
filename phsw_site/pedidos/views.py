24f66478a8f2eb263d8f3e898ec35cf2dd569969
Pedro Madureira <dev@phsolucoesweb.com.br>
2021-08-17 11:57:47 -0300

    Implementado paginação na pagina de pedidos.

--- phsw_site/pedidos/views.py
+++ phsw_site/pedidos/views.py
@@ -1,14 +1,22 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from . import facade
+from django.core.paginator import Paginator
 
 
 @login_required
 def pedidos(request):
     if request.user.is_agente_admin:
         pedidos_do_usuario = facade.buscar_pedidos_do_agente_admin(request)
-        return render(request, 'pedidos/pedidos_agente_admin.html', context={'pedidos': pedidos_do_usuario})
+        paginator = Paginator(pedidos_do_usuario, 3)
+        page = request.GET.get('page')
+        pedidos_paginator = paginator.get_page(page)
+
+        return render(request, 'pedidos/pedidos_agente_admin.html', context={'pedidos': pedidos_paginator})
     pedidos_do_usuario = facade.buscar_pedidos_do_usuario(request)
-    return render(request, 'pedidos/pedidos.html', context={'pedidos': pedidos_do_usuario})
+    paginator = Paginator(pedidos_do_usuario, 3)
+    page = request.GET.get('page')
+    pedidos_paginator = paginator.get_page(page)
+    return render(request, 'pedidos/pedidos.html', context={'pedidos': pedidos_paginator})
 
 
